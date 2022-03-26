e12c8749455bd1d401ddb056ba2bfbea766b8de1
Jason.Chang <chaoyue.chang@gmail.com>
2022-03-26 18:42:04 +0800

    :hammer: chore: 删除 jest config ,将 commitlint 配置移入 package.json

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  rootDir: './',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
-  testPathIgnorePatterns: ['./node_modules/'],
-  transformIgnorePatterns: ['./node_modules/'],
-}
