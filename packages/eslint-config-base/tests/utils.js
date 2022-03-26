63f9fd797b21406fe010e79176c90bf81d4217ec
Jason.Chang <chaoyue.chang@gmail.com>
2022-03-26 13:17:21 +0800

    :hammer: chore: 删除 filePath 避免在不同机器/部署目录，测试比对过程报错

--- packages/eslint-config-base/tests/utils.js
+++ packages/eslint-config-base/tests/utils.js
@@ -6,14 +6,18 @@ const test = require('tape');
 module.exports.runTest = function(pkgName, testCaseDir, testFileName = './bad.ts') {
   test(`Test pkg: "${pkgName}" eslint config can be used.`, async t => {
     const snapFile = path.resolve(testCaseDir, './results.snap.json');
-    const snap = JSON.parse(fs.readFileSync(snapFile));
+    let snap = JSON.parse(fs.readFileSync(snapFile));
 
     const cli = new ESLint({
       useEslintrc: false,
       baseConfig: require(path.resolve(testCaseDir, './rc.js')),
     });
 
-    const results = await cli.lintFiles([path.resolve(testCaseDir, testFileName)]);
+    let results = await cli.lintFiles([path.resolve(testCaseDir, testFileName)]);
+    results = results.map(i => {
+      i.filePath = ''; // 清空 filePath 避免不同机器/部署目录 造成比对过程报错
+      return i;
+    });
 
     fs.writeFileSync(path.resolve(testCaseDir, 'now.snap.json'), JSON.stringify(results, null, 2));
 
@@ -26,8 +30,11 @@ module.exports.updateSnap = async function(pkgName, testCaseDir, testFileName = 
     useEslintrc: false,
     baseConfig: require(path.resolve(testCaseDir, './rc.js')),
   });
-
-  const results = await cli.lintFiles([path.resolve(testCaseDir, testFileName)]);
+  let results = await cli.lintFiles([path.resolve(testCaseDir, testFileName)]);
+  results = results.map(i => {
+    i.filePath = ''; // 清空 filePath 避免不同机器/部署目录 造成比对过程报错
+    return i;
+  });
   const filename = path.resolve(testCaseDir, './results.snap.json');
   fs.writeFileSync(filename, JSON.stringify(results, null, 2));
 };
